d6ae04babd5d8b0e63b635eac434a61990d81c5c
Dawid Dębowski <dawid199960@gmail.com>
2021-07-28 16:25:37 +0200

    setup env

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DB_PASS } from '@env'
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{DB_PASS}</Text>
       <StatusBar style="auto" />
     </View>
   );
