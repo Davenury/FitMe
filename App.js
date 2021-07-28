5b56fbe9f2e6e9fe1ee984dbd22f973bfcb73961
Dawid Dębowski <dawid199960@gmail.com>
2021-07-28 16:54:45 +0200

    firebase initialized

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { DB_PASS } from '@env'
+import { API_KEY, AUTH_DOMAIN, STORAGE_BUCKET, PROJECT_ID, APP_ID, MESSAGING_SENDER_ID } from '@env'
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseUrl: `https://${PROJECT_ID}.firebaseio.com`,
+  storageBucket: STORAGE_BUCKET,
+  projectId: PROJECT_ID,
+  appId: APP_ID,
+  messagingSenderId: MESSAGING_SENDER_ID
+}
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
 
 export default function App() {
 
+  db.collection('recipes').doc('first').set({'x': 'da'})
+
   return (
     <View style={styles.container}>
-      <Text>{DB_PASS}</Text>
+      <Text>XD</Text>
       <StatusBar style="auto" />
     </View>
   );
