ca721a3d8bc39aa3511d9a8e0bcf0b665bd78475
Dawid Dębowski <dawid199960@gmail.com>
2021-07-28 18:56:42 +0200

    theme

--- App.js
+++ App.js
@@ -2,15 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { getAllRecipes } from './utils/database_client';
+import { createTheme, ThemeProvider, Button } from '@material-ui/core';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2569A1'
+    }
+  }
+})
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Text>XD</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>XD</Text>
+        <Button variant="contained" color="primary">Text</Button>
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
